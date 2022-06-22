de60f2e7e016a17c17a71c618a60fdd97674f85e
its-me-nishmal <twalaba123@gmail.com>
2022-06-22 12:00:00 +0000

    My Birthday😎

--- date.js
+++ date.js
@@ -19,7 +19,7 @@ async function commitAndPushToGit() {
         process.chdir('./');
 
         // Set the date for the commit (in ISO 8601 format)
-        const commitDate = '2022-06-21T12:00:00';
+        const commitDate = '2022-06-22T12:00:00';
         
         // Add files to staging area
         await gitCommand('git add .');
