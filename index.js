30b878854d50c8d86d8252988531d145a638a4a4
its-me-nishmal <twalaba123@gmail.com>
2024-01-20 14:43:06 +0530

    Fix a bug

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const simpleGit = require('simple-git');
 const path = require('path');
 const fs = require('fs');
-const localRepositoryPath = './GIT-BOT-V2'; // Local path to the existing repository
+const localRepositoryPath = './'; // Local path to the existing repository
 
 // Function to create a random commit message
 const getRandomCommitMessage = () => {
